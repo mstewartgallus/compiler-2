ad58cdf843963a10239aaa4373a82cfd2c80f949
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-09-24 17:01:36 -0700

    Simplify optimizations a bit

--- src/Cbpv/AsOpt.hs
+++ src/Cbpv/AsOpt.hs
@@ -15,42 +15,55 @@ import Prelude hiding ((.), id, curry, uncurry, Monad (..))
 
 data Stack f g (a :: Algebra) (b :: Algebra) where
   K :: f a b -> Stack f g a b
+  IdK :: Category f => Stack f g a a
+  Return :: Cbpv f g => g a b -> Stack f g (F a) (F b)
   Force :: Cbpv f g => g a (U b) -> Stack f g (F a) b
 
 data Code f g (a :: Set) (b :: Set) where
   C :: g a b -> Code f g a b
+  IdC :: Category g => Code f g a a
   Thunk :: Cbpv f g => f (F a) b -> Code f g a (U b)
 
 outC :: Code f g a b -> g a b
 outC expr = case expr of
   C x -> x
+  IdC -> id
   Thunk y -> thunk y
 
 outK :: Stack f g a b -> f a b
 outK expr = case expr of
   K x -> x
+  IdK -> id
+  Return x -> return x
   Force y -> force y
 
 opt :: Code f g a b -> g a b
 opt = outC
 
 instance (Category f, Category g) => Category (Stack f g) where
-  id = K id
+  id = IdK
+  IdK . f = f
+  f . IdK = f
+  Return f . Return g = Return (f . g)
   f . g = K (outK f . outK g)
 
 instance (Category f, Category g) => Category (Code f g) where
-  id = C id
+  id = IdC
+  IdC . f = f
+  f . IdC = f
   f . g = C (outC f . outC g)
 
 instance Cbpv f g => Cbpv (Stack f g) (Code f g) where
-  thunk (K f) = Thunk f
   thunk (Force f) = C f
+  thunk f = Thunk (outK f)
 
-  force (C f) = Force f
   force (Thunk f) = K f
+  force f = Force (outC f)
 
   f `to` x = K (outK f `to` outK x)
-  return f = K (return (outC f))
+
+  return IdC = IdK
+  return f = Return (outC f)
 
   unit = C unit
   f &&& g = C (outC f &&& outC g)
