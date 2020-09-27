7e71cc163d6ea03bafe417e39eb70949b3636011
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-09-26 18:00:02 -0700

    Cleanup add more

--- src/Cbpv/AsOpt.hs
+++ src/Cbpv/AsOpt.hs
@@ -58,13 +58,13 @@ instance Cbpv f g => Cbpv (Stack f g) (Code f g) where
 
 -- | fixme.. cleanup this mess
 addIntrinsic :: Cbpv stack code => stack (F Unit) (AsAlgebra ((Lambda.U64 Lambda.* Lambda.U64) Lambda.~> Lambda.U64))
-addIntrinsic = curry (bar . force id . return first . pop)
+addIntrinsic = curry (bar . push . force first . pop)
 
 foo :: Cbpv stack code => stack (F U64) (U (F U64) ~> F U64)
 foo = curry (uncurry (uncurry add . push . return (id &&& unit) . force id) . push . return (second &&& first) . pop)
 
-bar :: Cbpv stack code => stack (F (U (F U64) * U (F U64))) (F U64)
-bar = uncurry (foo . force id) . push
+bar :: Cbpv stack code => stack (U (F U64) & F (U (F U64))) (F U64)
+bar = uncurry (foo . force id)
 
 swap :: Cbpv stack code => code (a * b) (b * a)
 swap = second &&& first
