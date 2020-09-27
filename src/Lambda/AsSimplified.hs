5413510e2819f9ef54fde9d5d0e7a6f59425800b
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-09-27 12:03:58 -0700

    Cleanup simplifier

--- src/Lambda/AsSimplified.hs
+++ src/Lambda/AsSimplified.hs
@@ -18,7 +18,7 @@ import Lambda.Type
 import Prelude hiding ((.), id, curry, uncurry, Monad (..), Either (..))
 
 simplify :: Expr f a b -> f a b
-simplify = out
+simplify x = out (simp x)
 
 data Expr f a b where
   E :: f a b -> Expr f a b
@@ -38,6 +38,57 @@ data Expr f a b where
 
   Curry :: HasExp f => Expr f (a * env) b -> Expr f env (a ~> b)
   Uncurry :: HasExp f => Expr f env (a ~> b) -> Expr f (a * env) b
+
+simp :: Expr f a b -> Expr f a b
+simp expr = case opt expr of
+  Just x -> simp x
+  Nothing -> recurse expr
+
+opt :: Expr f a b -> Maybe (Expr f a b)
+opt expr  = case expr of
+  Fanout First Second -> Just id
+  Fanin Left Right -> Just id
+
+  Compose (Curry f) g -> Just $ curry (f . (first &&& (g . second)))
+  Uncurry (Compose f g) -> Just $ uncurry f . (first &&& (g . second))
+
+  Compose Id f -> Just f
+  Compose f Id -> Just f
+
+  Compose _ Absurd -> Just absurd
+  Compose (Fanin f _) Left -> Just f
+  Compose (Fanin _ f) Right -> Just f
+
+  Compose Unit _ -> Just unit
+  Compose First (Fanout f _) -> Just f
+  Compose Second (Fanout _ f) -> Just f
+
+  Compose x (Fanin f g) -> Just ((x . f) ||| (x . g))
+  Compose (Fanout f g) x -> Just ((f . x) &&& (g . x))
+
+  Compose (Compose f g) h  -> Just (f . (g . h))
+
+  _ -> Nothing
+
+recurse :: Expr f a b -> Expr f a b
+recurse expr = case expr of
+  E x -> E x
+  Id -> id
+
+  Compose f g -> simp f . simp g
+
+  Unit -> unit
+  First -> first
+  Second -> second
+  Fanout f g -> simp f &&& simp g
+
+  Absurd -> absurd
+  Left -> left
+  Right -> right
+  Fanin f g -> simp f ||| simp g
+
+  Curry f -> curry (simp f)
+  Uncurry f -> uncurry (simp f)
 
 out :: Expr f a b -> f a b
 out expr = case expr of
@@ -60,44 +111,23 @@ out expr = case expr of
 
 instance Category f => Category (Expr f) where
   id = Id
-  Id . f = f
-  f . Id = f
-
-  _ . Absurd = absurd
-  Fanin f _  . Left = f
-  Fanin _ f . Right = f
-
-  Unit . _ = unit
-  First . Fanout f _ = f
-  Second . Fanout _ f = f
-
-  x . Fanin f g = (x . f) ||| (x . g)
-  Fanout f g . x = (f . x) &&& (g . x)
-
-  Compose f g . h  = f . (g . h)
-  f . g = Compose f g
+  (.) = Compose
 
 instance HasProduct f => HasProduct (Expr f) where
   unit = Unit
-  First &&& Second = id
-  f &&& g = Fanout f g
+  (&&&) = Fanout
   first = First
   second = Second
 
 instance HasSum f => HasSum (Expr f) where
   absurd = Absurd
-  Left ||| Right = id
-  f ||| g = Fanin f g
+  (|||) = Fanin
   left = Left
   right = Right
 
 instance HasExp f => HasExp (Expr f) where
-  curry (Uncurry f) = f
-  curry f = Curry f
-
-  uncurry (Compose f g) = uncurry f . (first &&& (g . second))
-  uncurry (Curry f) = f
-  uncurry f = Uncurry f
+  curry = Curry
+  uncurry = Uncurry
 
 instance Lambda f => Lambda (Expr f) where
   u64 x = E (u64 x)
