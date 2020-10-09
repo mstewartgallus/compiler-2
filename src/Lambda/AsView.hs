46dc385689c071d793effaf049138eca9cdd6264
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-10-09 12:20:06 -0700

    Use State to print vars for hoas

--- src/Lambda/AsView.hs
+++ src/Lambda/AsView.hs
@@ -4,46 +4,54 @@
 module Lambda.AsView (View, view) where
 
 import Control.Category
-import Lambda.HasExp
+import Lambda.HasExp hiding ((<*>))
 import Lambda
 import Lambda.HasProduct
 import Lambda.HasSum
 import Lambda.HasLet
 import Lambda.Type
+import Control.Monad.State
 
-newtype View (a :: T) (b :: T) = V String
+newtype View (a :: T) (b :: T) = V (State Int String)
 
 view :: View a b -> String
-view (V v) = v
+view (V v) = evalState v 0
 
 instance Category View where
-  id = V "id"
-  V f . V g = V (f ++ " ∘ " ++ g)
+  id = V $ pure "id"
+  V f . V g = V $ pure (\f' g' -> f' ++ " ∘ " ++ g') <*> f <*> g
 
 instance HasProduct View where
-  unit = V "unit"
+  unit = V $ pure "unit"
 
-  V f &&& V x = V ("⟨" ++ f ++ " , " ++ x ++ "⟩")
-  first = V "π₁"
-  second = V "π₂"
+  V f &&& V g = V $ pure (\f' g' -> "⟨" ++ f' ++ " , " ++ g' ++ "⟩") <*> f <*> g
+  first = V $ pure "π₁"
+  second = V $ pure "π₂"
 
 instance HasSum View where
-  absurd = V "absurd"
+  absurd = V $ pure "absurd"
 
-  V f ||| V x = V ("[" ++ f ++ " , " ++ x ++ "]")
-  left = V "i₁"
-  right = V "i₂"
+  V f ||| V g = V $ pure (\f' g' -> "[" ++ f' ++ " , " ++ g' ++ "]") <*> f <*> g
+  left = V $ pure "i₁"
+  right = V $ pure "i₂"
 
 instance HasExp View where
-  curry (V f) = V ("(λ " ++ f ++ ")")
-  uncurry (V f) = V ("(! " ++ f ++ ")")
+  curry (V f) = V $ pure (\f' -> "(λ " ++ f' ++ ")") <*> f
+  uncurry (V f) = V $ pure (\f' -> "(! " ++ f' ++ ")") <*> f
 
 instance HasLet View where
-  be t f = V ("(be " ++ v ++ ": " ++ show t ++ ". " ++ body ++ ")") where
-    v = "v?"
-    V body = f (V v)
+  be t f = V $ do
+    v <- fresh
+    let V body = f (V $ pure v)
+    pure (\body' -> "(be " ++ v ++ ": " ++ show t ++ ". " ++ body' ++ ")") <*> body
 
 instance Lambda View where
-  u64 x = V (show x)
-  constant _ pkg name = V $ pkg ++ "/" ++ name
-  lambdaConstant _ pkg name = V $ "#" ++ pkg ++ "/" ++ name
+  u64 x = V $ pure (show x)
+  constant _ pkg name = V $ pure (pkg ++ "/" ++ name)
+  lambdaConstant _ pkg name = V $ pure ("#" ++ pkg ++ "/" ++ name)
+
+fresh :: State Int String
+fresh = do
+  n <- get
+  put (n + 1)
+  pure ("v" ++ show n)
