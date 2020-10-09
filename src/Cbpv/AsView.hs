6e3a62cfad1abb5a32cd6aedcf27f68f88732595
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-10-09 13:50:23 -0700

    Cleanup cbpv view

--- src/Cbpv/AsView.hs
+++ src/Cbpv/AsView.hs
@@ -18,18 +18,20 @@ view :: Code a b -> String
 view (C v) = evalState v 0
 
 instance Category Stack where
-  id = K $ pure "id"
-  K f . K g = K $ pure (\f' g' -> f' ++ " ∘ " ++ g') <*> f <*> g
+  id = K $ pure "pass"
+  K f . K g = K $ pure (\f' g' -> g' ++ ";\n" ++ f') <*> f <*> g
 
 instance Category Code where
   id = C $ pure "id"
   C f . C g = C $ pure (\f' g' -> f' ++ " ∘ " ++ g') <*> f <*> g
 
-instance Cbpv Stack Code where
-  return (C f) = K $ pure (\f' -> "(return " ++ f' ++ ")") <*> f
+indent = unlines . map ("\t" ++) . lines
 
-  thunk (K f) = C $ pure (\f' -> "(thunk " ++ f' ++ ")") <*> f
-  force (C f) = K $ pure (\f' -> "(force " ++ f' ++ ")") <*> f
+instance Cbpv Stack Code where
+  return (C f) = K $ pure (\f' -> "return " ++ f' ++ "") <*> f
+
+  thunk (K f) = C $ pure (\f' -> "thunk {" ++ indent ("\n" ++ f')  ++ "}") <*> f
+  force (C f) = K $ pure (\f' -> "force " ++ f' ++ "") <*> f
 
   unit = C $ pure "unit"
   C f &&& C g = C $ pure (\f' g' -> "⟨" ++ f' ++ ", " ++ g' ++ "⟩") <*> f <*> g
@@ -44,20 +46,20 @@ instance Cbpv Stack Code where
   pop = K $ pure "pop"
   push = K $ pure "push"
 
-  curry (K f) = K $ pure (\f' -> "(λ " ++ f' ++ ")") <*> f
-  uncurry (K f) = K $ pure (\f' -> "(! " ++ f' ++ ")") <*> f
+  curry (K f) = K $ pure (\f' -> "λ\n" ++ f' ++ "") <*> f
+  uncurry (K f) = K $ pure (\f' -> "!\n" ++ f' ++ "") <*> f
 
   be (C x) f = C $ do
     v <- fresh
     let C body = f (C $ pure v)
     body' <- body
-    pure (\x' body' -> "(" ++ x' ++ " be " ++ v ++ ".\n" ++ body' ++ ")") <*> x <*> body
+    pure (\x' body' -> "" ++ x' ++ " be " ++ v ++ ".\n" ++ body' ++ "") <*> x <*> body
 
   letTo (K x) f = K $ do
     v <- fresh
     let K body = f (C $ pure v)
     body' <- body
-    pure (\x' body' -> "(" ++ x' ++ " to " ++ v ++ ".\n" ++ body' ++ ")") <*> x <*> body
+    pure (\x' body' -> "" ++ x' ++ " to " ++ v ++ ".\n" ++ body' ++ "") <*> x <*> body
 
   u64 x = C $ pure (show x)
   constant _ pkg name = K $ pure (pkg ++ "/" ++ name)
