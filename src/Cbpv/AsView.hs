5db07d984e0ae54d4ab1907bdbd327e4bc4d5968
Steven Stewart-Gallus <sstewartgallus@sstewartgallus.com>
2020-12-24 15:02:22 -0800

    Simplify cbpv with polykinds over sorts

--- src/Cbpv/AsView.hs
+++ src/Cbpv/AsView.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 
-module Cbpv.AsView (Stack, view) where
+module Cbpv.AsView (View, view) where
 
 import Cbpv
 import Control.Category
@@ -10,56 +12,50 @@ import Control.Monad.State
 import Cbpv.Sort
 import Prelude hiding ((.), id)
 
-newtype Stk (a :: Algebra) (b :: Algebra) = K (State Int String)
+newtype View (a :: Sort t) (b :: Sort t) = V (State Int String)
 
-newtype Cde (a :: Set) (b :: Set) = C (State Int String)
+view :: View a b -> String
+view (V v) = evalState v 0
 
-view :: Cde a b -> String
-view (C v) = evalState v 0
-
-instance Category Stk where
-  id = K $ pure "pass"
-  K f . K g = K $ pure (\f' g' -> g' ++ " ;\n" ++ f') <*> f <*> g
-
-instance Category Cde where
-  id = C $ pure "id"
-  C f . C g = C $ pure (\f' g' -> "(" ++ f' ++ " ∘ " ++ g' ++ ")") <*> f <*> g
+instance Category View where
+  id = V $ pure "id"
+  V f . V g = V $ pure (\f' g' -> "(" ++ f' ++ " ∘ " ++ g' ++ ")") <*> f <*> g
 
 indent = unlines . map ("\t" ++) . lines
 
-instance Code Cde where
-  unit = C $ pure "unit"
+instance Code View where
+  unit = V $ pure "unit"
 
-  lift (C f) = C $ pure (\f' -> "(lift " ++ f' ++ ")") <*> f
-  kappa _ f = C $ do
+  lift (V f) = V $ pure (\f' -> "(lift " ++ f' ++ ")") <*> f
+  kappa _ f = V $ do
     v <- fresh
-    let C body = f (C $ pure v)
+    let V body = f (V $ pure v)
     pure (\body' -> "(κ " ++ v ++ ". " ++ body' ++ ")") <*> body
 
-instance Stack Stk where
+instance Stack View where
 
-instance Cbpv Stk Cde where
-  thunk (K f) = C $ pure (\f' -> "thunk {" ++ indent ("\n" ++ f')  ++ "}") <*> f
-  force (C f) = K $ pure (\f' -> "(force " ++ f' ++ ")") <*> f
+instance Cbpv View View where
+  thunk (V f) = V $ pure (\f' -> "thunk {" ++ indent ("\n" ++ f')  ++ "}") <*> f
+  force (V f) = V $ pure (\f' -> "(force " ++ f' ++ ")") <*> f
 
-  pass (C f) = K $ pure (\f' -> "(pass " ++ f' ++ ")") <*> f
-  push (C f) = K $ pure (\f' -> "(push " ++ f' ++ ")") <*> f
+  pass (V f) = V $ pure (\f' -> "(pass " ++ f' ++ ")") <*> f
+  push (V f) = V $ pure (\f' -> "(push " ++ f' ++ ")") <*> f
 
-  zeta t f = K $ do
+  zeta t f = V $ do
     v <- fresh
-    let K body = f (C $ pure v)
+    let V body = f (V $ pure v)
     body' <- body
     pure (\body' -> "(ζ " ++ v ++ ": " ++ "-" ++ ".\n" ++ body' ++ ")") <*> body
-  pop t f = K $ do
+  pop t f = V $ do
     v <- fresh
-    let K body = f (C $ pure v)
+    let V body = f (V $ pure v)
     body' <- body
     pure (\body' -> "(pop " ++ v ++ ".\n" ++ body' ++ ")") <*> body
 
-  u64 x = C $ pure (show x)
-  constant _ pkg name = K $ pure (pkg ++ "/" ++ name)
-  cccIntrinsic x = C $ pure (show x)
-  cbpvIntrinsic x = C $ pure (show x)
+  u64 x = V $ pure (show x)
+  constant _ pkg name = V $ pure (pkg ++ "/" ++ name)
+  cccIntrinsic x = V $ pure (show x)
+  cbpvIntrinsic x = V $ pure (show x)
 
 fresh :: State Int String
 fresh = do
